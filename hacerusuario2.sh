e9458fdbd5781a663a97fe1a6fdee64d0710ccbe
Tu Nombre <you@example.com>
2024-05-20 09:42:31 +0200

    mejoria?

--- hacerusuario2.sh
+++ hacerusuario2.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-if [[ ${UID} -ne  0 ]]
-then
+# Verifica si el usuario es root
+if [[ ${UID} -ne 0 ]]; then
     echo "no ets root"
-    exit
+    exit 1
 fi
 
-
-#parametros obligatorios
-
+# Solicita los parámetros obligatorios
 read -p "Introduce el nombre completo: " FULL_NAME
-read -p "Introduce el nombre de usuario : " USERNAME
+read -p "Introduce el nombre de usuario: " USERNAME
 
-PASSWORD=$(date +%s%N | sha256sum |head -c10)
+# Genera una contraseña aleatoria
+PASSWORD=$(date +%s%N | sha256sum | head -c10)
 
 echo "Creant usuari ${USERNAME}"
-useradd -c  "${FULL_NAME}" -m ${USERNAME}
-
-if [[ ${?} -ne 0 ]]
-then
+useradd -c "${FULL_NAME}" -m ${USERNAME}
+# Verifica si el usuario fue creado correctamente
+if [[ ${?} -ne 0 ]]; then
     echo "Error al crear el usuari"
     exit 1
 fi
-echo "${USERNAME}:${PASSWORD}" | chpasswd
 
-if [[ ${?} -ne 0 ]]
-then
+# Asigna la contraseña al usuario
+echo "${USERNAME}:${PASSWORD}" | chpasswd
+# Verifica si la contraseña fue asignada correctamente
+if [[ ${?} -ne 0 ]]; then
     echo "Error al crear la contrasenya"
     exit 1
 fi
 
+
 passwd -e ${USERNAME}
+# Verifica si el comando anterior fue exitoso
+if [[ ${?} -ne 0 ]]; then
+    echo "Error al expirar la contrasenya"
+    exit 1
+fi
 
 echo "Usuari ${USERNAME} creat correctament"
 echo "Contraseña: ${PASSWORD}"
 echo "Host: ${HOSTNAME}"
+
 exit 0
