5a93bfd4cca75098a5d889e0085a916e7a8af825
Tu Nombre <you@example.com>
2024-06-05 13:58:45 +0200

    Script nuevo de práctica

---
+++ examen.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#read -p "Dime tu nombre: " NOM
+
+#echo "Hola $NOM"
+
+#####################################################33
+
+
+#read -p "Dime el número del cual quieres su factorial: " NUM
+
+#FAC=1
+#AAA=1
+
+#while [[ $AAA -le $NUM ]]
+#do
+ #   FAC=$((FAC*AAA))
+#    AAA=$((AAA+1))
+#    echo "$AAA * $FAC"
+#done
+
+#echo "El factorial de $NUM es: $FAC"
+
+################################################################
+
+#read -p "Di un numero: " NUMERITO
+
+
+#if (( NUMERITO % 2 == 0 )); then
+ # echo "$NUMERITO es un número par."
+#else
+ # echo "$NUMERITO es un número impar."
+#fi
+
+##############################################
+
+#RANDOMM=$((RANDOM % 100+1))
+
+#echo "numeiro es: $RANDOMM"
+
+
+######################################
+
+#read -p "Di un numero: " NUME
+#read -p "Di un numero: " NUMER
+#SUMA=$((NUME+NUMER))
+
+#echo "La suma de estos dos numeros es $SUMA"
+
+########################################
+
